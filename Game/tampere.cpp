55d2a2333306fbf50c9aa95247eb5a6aa4d99dfc
Ilari Räisänen <ilari.raisanen@tuni.fi>
2020-11-11 10:52:34 +0200

    corrected coordinates

--- Game/tampere.cpp
+++ Game/tampere.cpp
@@ -64,7 +64,7 @@ void Tampere::actorMoved(std::shared_ptr<Interface::IActor> actor){
    std::map<std::shared_ptr<Interface::IActor>,CourseSide::SimpleActorItem*>::iterator it;
    it = nysse_graphic_pairs.find(actor);
    if(it != nysse_graphic_pairs.end()){
-        it->second->setCoord(actor.get()->giveLocation().giveX(),actor.get()->giveLocation().giveY());
+        it->second->setPos(QPoint(actor.get()->giveLocation().giveX(),actor.get()->giveLocation().giveY()));
    }
 }
 std::vector<std::shared_ptr<Interface::IActor>> Tampere::getNearbyActors(Interface::Location loc) const {
@@ -76,10 +76,11 @@ bool Tampere::isGameOver() const {
 
 void Tampere::drawNysses(){
     for(std::shared_ptr<Interface::IActor> bus : nysses){
-        CourseSide::SimpleActorItem* nysse = new CourseSide::SimpleActorItem(bus->giveLocation().giveX(),
-                                                                             bus->giveLocation().giveY(),255);
+        QPoint coords = QPoint(bus->giveLocation().giveX(),bus->giveLocation().giveY());
+        CourseSide::SimpleActorItem* nysse = new CourseSide::SimpleActorItem(0,0,255);
         nysse_graphic_pairs.insert({bus, nysse});
         scene->addItem(nysse);
+        nysse->setPos(coords);
     }
 }
 
